60e51cb12a1e47e2f1e4f945ff3d4f9c622c4d1e
Mohit B V <bvmohituppsala@gmail.com>
2017-04-04 03:22:42 +0200

    Add files via upload
    depending on the button state Led's count from 000(all low) to 111(all high)

---
+++ _000to111.ino
@@ -0,0 +1,97 @@
+/*
+  Button
+
+ Turns on and off a light emitting diode(LED) connected to digital
+ pin 13, when pressing a pushbutton attached to pin 2.
+
+
+ The circuit:
+ * LED attached from pin 13 to ground
+ * pushbutton attached to pin 2 from +5V
+ * 10K resistor attached to pin 2 from ground
+
+ * Note: on most Arduinos there is already an LED on the board
+ attached to pin 13.
+
+
+ created 2005
+ by DojoDave <http://www.0j0.org>
+ modified 30 Aug 2011
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/Button
+ */
+
+// constants won't change. They're used here to
+// set pin numbers:
+const int buttonPin = 3;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+const int ledPin1 =  9; 
+const int ledPin2 =  10; 
+const int ledPin3 =  5; 
+const int buttonPin1 = 12;
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+int buttonstate1 = 0;
+void setup() {
+  // initialize the LED pin as an output:
+  //pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  pinMode(13, OUTPUT);
+  pinMode(buttonPin1, INPUT);
+   pinMode(9, OUTPUT);
+   pinMode(10, OUTPUT);
+    pinMode(5, OUTPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin2, LOW); 
+    digitalWrite(ledPin1, LOW);
+    delay(1000);
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin2, LOW); 
+    digitalWrite(ledPin1, HIGH);
+    delay(1000);
+    digitalWrite(ledPin3, LOW);
+    digitalWrite(ledPin2, HIGH); 
+    digitalWrite(ledPin1, LOW);
+    delay(1000);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin2, LOW); 
+    digitalWrite(ledPin1, LOW);
+    delay(1000);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin2, LOW); 
+    digitalWrite(ledPin1, HIGH);
+    delay(1000);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin2, HIGH); 
+    digitalWrite(ledPin1, LOW);
+    delay(1000);
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin2, HIGH); 
+    digitalWrite(ledPin1, HIGH);
+    delay(1000);
+  }
+ 
+  
+  else {
+    // turn LED off:
+    
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin3, LOW);
+    delay(2000);
+  }
+}
